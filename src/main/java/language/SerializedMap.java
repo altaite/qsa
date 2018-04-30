46240025ab153218e37d6a31b389347def0e420c
altaite <antonin.pavelka@gmail.com>
2018-04-30 13:22:20 -0700

    serialized map started

---
+++ src/main/java/language/SerializedMap.java
@@ -0,0 +1,25 @@
+package language;
+
+/**
+ *
+ * @author Antonin Pavelka
+ */
+public class SerializedMap<K, T> {
+	//TODO generics as map, maybe copy interface
+	
+	// save the map itself
+	// maintain mapping to integer id for sake of compression, refs from biword pairs
+	
+	public SerializedMap() {
+		
+	}
+	
+	public T get(K id) {
+		
+	}
+	
+	public T get(int index) {
+		
+	}
+	
+}
